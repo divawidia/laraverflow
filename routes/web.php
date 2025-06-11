22f8413ba3d651c54c78b5bae36335ba8a702cc4
divawidia <wiartha2001@gmail.com>
2025-06-11 00:00:00 +0000

    refactor (web routes): update question routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Http\Resources\QuestionResource;
+use App\Models\Question;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return \App\Http\Resources\QuestionResource::collection(\App\Models\Question::with('user')->latest()->paginate(15));
+    $questions = QuestionResource::collection(Question::with('user')->latest()->paginate(15));
+    return Inertia::render('Questions/Index', [
+        'questions' => $questions,
+    ]);
 })->name('questions.index');
 
-Route::get('/questions/{id}', function ($id) {
+Route::get('/questions/{question:slug}', function (Question $question) {
     return Inertia::render('Questions/Show', [
-        'question' => [
-            'id' => $id,
-            'title'=> 'Question 1',
-        ]
+        'question' => QuestionResource::make($question),
     ]);
 })->name('questions.show');
