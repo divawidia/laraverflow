530977a37087de76099355d8eb097c9dfe359db8
divawidia <wiartha2001@gmail.com>
2025-06-12 00:00:00 +0000

    feat: create question controller

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\QuestionResource;
+use App\Models\Question;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        $questions = QuestionResource::collection(Question::with('user')->latest()->paginate(15));
+        return Inertia::render('Questions/Index', [
+            'questions' => $questions,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Question $question): Response
+    {
+        return Inertia::render('Questions/Show', [
+            'question' => QuestionResource::make($question),
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Question $question)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Question $question)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
-use App\Http\Resources\QuestionResource;
-use App\Models\Question;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    $questions = QuestionResource::collection(Question::with('user')->latest()->paginate(15));
-    return Inertia::render('Questions/Index', [
-        'questions' => $questions,
-    ]);
-})->name('questions.index');
-
-Route::get('/questions/{question:slug}', function (Question $question) {
-    return Inertia::render('Questions/Show', [
-        'question' => QuestionResource::make($question),
-    ]);
-})->name('questions.show');
+Route::get('/', [QuestionController::class, 'index'])->name('questions.index');
+Route::get('questions/{question:slug}', [QuestionController::class, 'show'])->name('questions.show');
