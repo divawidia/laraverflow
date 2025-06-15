ca90ca991baf7fe4cfe83848673505bf9f5cb224
divawidia <wiartha2001@gmail.com>
2025-06-15 00:00:00 +0000

    feat (Question): add questions resource routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [QuestionController::class, 'index'])->name('questions.index');
 Route::get('questions/{question:slug}', [QuestionController::class, 'show'])->name('questions.show');
+Route::resource('questions', QuestionController::class)
+    ->except(['index', 'show'])
+    ->middleware(['auth']);
