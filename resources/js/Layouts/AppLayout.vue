0d158e724cb63f3cc7eb076b814cbf04a7a21ae8
divawidia <wiartha2001@gmail.com>
2025-06-18 00:00:00 +0000

    feat (Toas Component): add toas component

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,13 +1,29 @@
 <script setup>
+import {onMounted, reactive} from "vue";
+import * as bootstrap from 'bootstrap';
 import Navbar from "../Components/Navbar.vue";
+import Toast from "../Components/Toast.vue";
+
+const state = reactive({
+    toastRef: null
+})
+
+onMounted(() => {
+    state.toastRef = new bootstrap.Toast('#app-toast', {
+        delay: 3000,
+    })
+})
+
+const showToast = () => state.toastRef.show()
 </script>
 
 <template>
     <Navbar />
-
+    <button class="btn btn-success" @click="showToast()">Show Toast</button>
     <main class="py-4">
         <slot/>
     </main>
+    <Toast id="app-toast"/>
 </template>
 
 <style scoped>
